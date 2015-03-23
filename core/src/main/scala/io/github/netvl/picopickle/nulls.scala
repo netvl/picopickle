14d57619f76cad29dac0cbd9c7aa059318c87870
Vladimir Matveev <vladimir.matweev@gmail.com>
2015-03-24 00:54:15 +0300

    Added support for nulls handling

---
+++ core/src/main/scala/io/github/netvl/picopickle/nulls.scala
@@ -0,0 +1,49 @@
+package io.github.netvl.picopickle
+
+trait NullHandlerComponent {
+  this: TypesComponent with BackendComponent =>
+
+  def nullHandler: NullHandler
+
+  trait NullHandler {
+    def handlesNull: Boolean
+    def toBackend[T](value: T, cont: T => backend.BValue): backend.BValue
+    def fromBackend[T](value: backend.BValue, cont: backend.BValue => T): T
+  }
+}
+
+trait DefaultNullHandlerComponent extends NullHandlerComponent {
+  this: TypesComponent with BackendComponent =>
+
+  def nullHandler: NullHandler = new NullHandler {
+    override def handlesNull: Boolean = true
+
+    override def fromBackend[T](value: backend.BValue, cont: backend.BValue => T): T = value match {
+      case backend.Get.Null(_) => null.asInstanceOf[T]
+      case _ => cont(value)
+    }
+
+    override def toBackend[T](value: T, cont: T => backend.BValue): backend.BValue = value match {
+      case null => backend.makeNull
+      case _ => cont(value)
+    }
+  }
+}
+
+trait ProhibitiveNullHandlerComponent extends NullHandlerComponent {
+  this: TypesComponent with BackendComponent =>
+
+  def nullHandler: NullHandler = new NullHandler {
+    override def handlesNull: Boolean = false
+
+    override def fromBackend[T](value: backend.BValue, cont: backend.BValue => T): T = value match {
+      case backend.Get.Null(_) => throw new IllegalArgumentException("null values are prohibited")
+      case _ => cont(value)
+    }
+
+    override def toBackend[T](value: T, cont: T => backend.BValue): backend.BValue = value match {
+      case null => throw new IllegalArgumentException("null values are prohibited")
+      case _ => cont(value)
+    }
+  }
+}
